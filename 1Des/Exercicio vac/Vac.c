24d4f08e4b73e7144ddd6d62d7ebed2dbf914ceb
igorhenriqueg <igorhgoncalvess@gmail.com>
2023-08-07 11:02:35 -0300

    vac

---
+++ 1Des/Exercicio vac/Vac.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+	
+	int m, v; // Meses & vacinas
+	
+	printf("Meses: ");
+	scanf("%d", &m);
+	
+	prinf("Vacinas: ");
+	scanf("%d", &v);
+	
+	
+	
+	return 0;
+}
