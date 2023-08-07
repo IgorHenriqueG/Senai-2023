395dee72474bb2d0aede2032714c4af93fdf2d24
igorhgoncalvess <igorhgoncalvess@gmail.com>
2023-08-07 05:28:57 -0300

    atividades 02

--- 1Des/01-fpoo/Exercicíos/C/02/E01.c
+++ 1Des/01-fpoo/Exercicíos/C/02/E01.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 int main(void){
-	int preco;
+	float preco;
 	
-	printf("Entre o preco do produto: ");
-	scanf("%d", preco);
+	printf("Entre o preco do produto: R$");
+	scanf("%f", &preco);
 	
-//	if(preco > 1000){
-//		preco = preco * 1.05;
-//		printf("O preco do produto com desconto de 5%% e de %d", preco);
-//		return 0;
-//	} else{
-//		printf("O preco do produto nao sofreu alteracoes.");
-//	}
-	printf("O preco do p222roduto nao sofreu alteracoes.");
+	if(preco > 1000){
+		preco = preco * 0.92;
+		printf("O preco do produto com desconto de 8%% e de R$%.2f", preco);
+	} else{
+		printf("O preco do produto nao sofreu alteracoes.");
+	}
 	return 0;
 }
