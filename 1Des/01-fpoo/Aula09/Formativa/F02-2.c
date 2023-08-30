1184c398389df7e7ae9c68d08684d90a04e6d34c
IgorHenriqueG <igorhgoncalvess@gmail.com>
2023-08-30 11:26:42 -0300

    F02-2

---
+++ 1Des/01-fpoo/Aula09/Formativa/F02-2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+int main(void){
+	setlocale(LC_ALL,"");
+	char cidade[20];
+	int numCandidatos, i;
+	
+	
+	printf("Qual cidade a votação irá ocorrer: ");
+	scanf("%s", &cidade);
+	
+	printf("Quantos candidatos a cidade %s possui: ", cidade);
+	scanf("%d", &numCandidatos);
+	
+	char candidato[numCandidatos][20];
+	float porcentagem[numCandidatos], votos[numCandidatos], totalVotos, brancos, nulos;
+	
+	for(i = 1; i <= numCandidatos; i++){
+		printf("\nInsira o nome do candidato %d: ", i);
+		scanf("%s", &candidato[i]);
+		
+		printf("Quantos votos o candidato %s teve: ", candidato[i]);
+		scanf("%f", &votos[i]);
+	}
+	
+	printf("\nQuantos votos em branco houveram: ");
+	scanf("%f", &brancos);
+	
+	printf("Quantos votos nulos houveram: ");
+	scanf("%f", &nulos);
+	
+	for(i = 1; i <= numCandidatos; i++){
+		totalVotos = totalVotos + votos[i];	
+	}
+	
+	totalVotos = totalVotos + brancos;
+	
+	for(i = 1; i <= numCandidatos; i++){
+		porcentagem[i] = (votos[i] / totalVotos) * 100;
+	}
+		
+	if(totalVotos > nulos){
+		printf("\n\nVotos válidos: %.0f\nVotos Inválidos: %.0f", totalVotos, nulos);
+		printf("\n\nCandidatos\tPorcentagem");
+		for(i = 1; i <= numCandidatos; i++){
+			printf("\n%s\t\t\t%.1f%%", candidato[i], porcentagem[i]);
+		}
+		brancos = (brancos / totalVotos) * 100;
+		printf("\nBrancos\t\t\t%.1f%%", brancos);
+		
+		if(totalVotos > 200000){
+			
+		}
+		
+			
+		
+	}else{
+		printf("Votação inválida");
+	}
+	
+	
+	return 0;
+}
