fbef5f36477ee67b96b3a0ff2b75693ad2c01bb8
igorhgoncalvess <igorhgoncalvess@gmail.com>
2023-09-12 11:16:24 -0300

    FPOO Lista 04

--- 1Des/01-fpoo/Exercicíos/C/04/E02.c
+++ 1Des/01-fpoo/Exercicíos/C/04/E02.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
 
 int main(void){
 	setlocale(LC_ALL,"");
 	char time[20][5], temp2[20];
 	int vitorias[20], empates[20], totalPontos[20], temp, i;
 	
-	for(i = 1; i <= 5; i++){
+	for(i = 0; i < 5; i++){
 		printf("\nNome do time: ");
 		scanf("%s", &time[i]);
-		time[i] + i;
-		
+				
 		printf("Vitórias do time %s: ", time[i]);
 		scanf("%d", &vitorias[i]);
-		vitorias[i] + i;
 		
 		printf("Empates do time %s: ", time[i]);
 		scanf("%d", &empates[i]);
-		empates[i] + i;
 	}
 	
-	for(i = 1; i <= 5; i++){
+	for(i = 0; i < 5; i++){
 		vitorias[i] = vitorias[i] * 3;
 		
 		
@@ -31,47 +29,51 @@ int main(void){
 		
 	}
 	
-	for(i = 1; i <= 5; i++){
+	for(i = 0; i < 25; i++){
+		if(totalPontos[0] < totalPontos[1]){
+			strcpy(temp2, time[0]);
+			strcpy(time[0], time[1]);
+			strcpy(time[1], temp2);
+
+			temp = totalPontos[0];
+			totalPontos[0] = totalPontos[1];
+			totalPontos[1] = temp;
+		}
+		
 		if(totalPontos[1] < totalPontos[2]){
-			temp2 = time[20][1];
-			time[20][1] = time[20][2];
-			time[20][2] = temp2;
-			
+			strcpy(temp2, time[1]);
+			strcpy(time[1], time[2]);
+			strcpy(time[2], temp2);
+
 			temp = totalPontos[1];
 			totalPontos[1] = totalPontos[2];
 			totalPontos[2] = temp;
 		}
+		
 		if(totalPontos[2] < totalPontos[3]){
-			temp2 = time[20][2];
-			time[20][2] = time[20][3];
-			time[20][3] = temp2;
-			
+			strcpy(temp2, time[2]);
+			strcpy(time[2], time[3]);
+			strcpy(time[3], temp2);
+
 			temp = totalPontos[2];
 			totalPontos[2] = totalPontos[3];
 			totalPontos[3] = temp;
 		}
+		
 		if(totalPontos[3] < totalPontos[4]){
-			temp2 = time[20][3];
-			time[20][3] = time[20][4];
-			time[20][4] = temp2;
-			
+			strcpy(temp2, time[3]);
+			strcpy(time[3], time[4]);
+			strcpy(time[4], temp2);
+
 			temp = totalPontos[3];
 			totalPontos[3] = totalPontos[4];
 			totalPontos[4] = temp;
 		}
-		if(totalPontos[4] < totalPontos[5]){
-			temp2 = time[20][4];
-			time[20][4] = time[20][5];
-			time[20][5] = temp2;
-			
-			temp = totalPontos[4];
-			totalPontos[4] = totalPontos[5];
-			totalPontos[5] = temp;
-		}
 	}
 	printf("\n");
-	for(i = 1; i <= 5; i++){
-		printf("%s\t%d\t%d\t%d\n", time[i], totalPontos[i], vitorias[i], empates[i]);
+	printf("\nTime: Pontos:\n");
+	for(i = 0; i < 5; i++){
+		printf("%s\t%d\n", time[i], totalPontos[i]);
 	}
 	
 	return 0;
