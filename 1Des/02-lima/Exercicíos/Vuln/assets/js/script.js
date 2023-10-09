bc912feb2ae56b671f8d6d686ddeb6dcfdf41425
isaccatarinosn <isaccatarinosn@gmail.com>
2023-10-09 11:18:30 -0300

    Seg

---
+++ 1Des/02-lima/Exercicíos/Vuln/assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+function vuln(){
+    let textInput = document.querySelector("#inputText")
+    let vuln = document.querySelector("#vuln")
+    let noVuln = document.querySelector("#noVuln")
+
+    vuln.innerHTML = textInput.value
+    noVuln.textContent = textInput.value
+}
