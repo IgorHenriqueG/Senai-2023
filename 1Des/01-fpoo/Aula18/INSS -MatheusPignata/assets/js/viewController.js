3a4d88b55c9d2698e4c5d42848c6374e4db14a28
JaqueJak <jaqueline.44rodrigues@gmail.com>
2023-10-19 11:25:12 -0300

    autogit

--- 1Des/01-fpoo/Aula18/INSS -MatheusPignata/assets/js/viewController.js
+++ 1Des/01-fpoo/Aula18/INSS -MatheusPignata/assets/js/viewController.js
@@ -19,12 +19,12 @@ function criarCard(){
     pessoas.forEach(pessoa => {
         let cardNew = card.cloneNode(true)
         cardNew.classList.remove('model')
-        cardNew.querySelector("#nome").innerHTML = pessoa.nome
-        cardNew.querySelector("#salario").innerHTML = `<p>Salário:</p><p>R$${pessoa.salario}</p>`
-        cardNew.querySelector("#inss").innerHTML = `<p>INSS:</p><p>R$${pessoa.inss().toFixed(2)}`
-        cardNew.querySelector("#salarioBase").innerHTML = `<p>Sal. Base:</p><p>R$${pessoa.salarioBase}`
-        cardNew.querySelector("#irrf").innerHTML = `<p>IRRF:</p><p>R$${pessoa.irrf().toFixed(2)}</p>`
-        cardNew.querySelector("#salarioLiquido").innerHTML = `<p>Sal. Liquido:</p><p>R$${pessoa.salarioLiquido}</p>`
+        cardNew.querySelector(".nome").innerHTML = pessoa.nome
+        cardNew.querySelector(".salario").innerHTML = `<p>Salário:</p><p>R$<span class="editSalarioInput">${pessoa.salario}</span></p>`
+        cardNew.querySelector(".inss").innerHTML = `<p>INSS:</p><p>R$${pessoa.inss().toFixed(2)}`
+        cardNew.querySelector(".salarioBase").innerHTML = `<p>Sal. Base:</p><p>R$${pessoa.salarioBase}`
+        cardNew.querySelector(".irrf").innerHTML = `<p>IRRF:</p><p>R$${pessoa.irrf().toFixed(2)}</p>`
+        cardNew.querySelector(".salarioLiquido").innerHTML = `<p>Sal. Liquido:</p><p>R$${pessoa.salarioLiquido}</p>`
 
         cards.appendChild(cardNew)
     })
@@ -33,4 +33,48 @@ function criarCard(){
 function remover(e){
     e.parentNode.remove()
     pessoas.splice(e, 1)
+}
+
+function edit(e){
+    let cardName = e.parentNode.querySelector('.name')
+    let cardSalary = e.parentNode.querySelector('.salario')
+
+    let nameInput = document.createElement('input')
+    let salaryInput = document.createElement('input')
+
+    let doneButton = document.createElement('button')
+    doneButton.innerText = 'Done'
+    doneButton.classList.add('doneButton')
+    doneButton.setAttribute('onclick', 'editDone(this)')
+
+
+    nameInput.classList.add('editInput')
+    nameInput.value = cardName.querySelector('.nome').innerHTML
+
+    cardName.innerHTML= ''
+    cardName.appendChild(nameInput)
+
+    salaryInput.classList.add('editInputSalary', 'editInput')
+    salaryInput.value = cardSalary.querySelector('.editSalarioInput').innerHTML
+
+    cardSalary.innerHTML= ''
+    cardSalary.appendChild(salaryInput)
+
+    e.parentNode.appendChild(doneButton)
+}
+
+function editDone(e){
+    let cardNameM = e.parentNode.querySelector('.name')
+    let cardSalaryM = e.parentNode.querySelector('.salario')
+
+    let inputName = e.parentNode.querySelector('.editInput')
+    let inputSalary = e.parentNode.querySelector('.editInputSalary')
+
+    cardNameM.innerHTML = `<h1 class="nome">${inputName.value}</h1>`
+    cardSalaryM.innerHTML = `<p>Salário:</p><p>R$<span class="editSalarioInput">${inputSalary.value}</span></p>`
+
+    e.parentNode.querySelector('.doneButton').remove()
+
+
+
 }
