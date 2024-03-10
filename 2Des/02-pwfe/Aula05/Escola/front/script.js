292c6c3fc668d213af0cab053b0474a5877a728e
igorhgoncalvess <igorhgoncalvess@gmail.com>
2024-03-10 14:18:57 -0300

    autogit

---
+++ 2Des/02-pwfe/Aula05/Escola/front/script.js
@@ -0,0 +1,37 @@
+// const firstSection = document.querySelector('.first');
+// const secondSection = document.querySelector('.second > div');
+
+// let backgroundY = 0
+
+// window.addEventListener('load', () => {
+//     backgroundY = secondSection.offsetTop + 15;
+
+//     console.log(backgroundY);
+// })
+
+// window.addEventListener('scroll', () => {
+//     const scroll = window.scrollY
+
+//     secondSection.style.top = backgroundY - scroll / 25 + 'vh'
+// })
+
+
+
+// Second Try
+
+const firstSection = document.querySelector('.first');
+const secondSection = document.querySelector('.second');
+
+let backgroundY = 0
+
+window.addEventListener('load', () => {
+    backgroundY = secondSection.offsetTop;
+})
+
+window.addEventListener('scroll', () => {
+    const scroll = window.scrollY
+
+    firstSection.style.top = scroll / 10 + 'px'
+
+    secondSection.style.top = backgroundY - scroll + 'px'
+})
