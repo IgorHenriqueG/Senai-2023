dfbb95496c2563162594a68883c629467c99cc91
igorhgoncalvess <igorhgoncalvess@gmail.com>
2024-03-10 19:30:42 -0300

    autogit

--- 2Des/02-pwfe/Aula05/Escola/front/script.js
+++ 2Des/02-pwfe/Aula05/Escola/front/script.js
@@ -19,19 +19,19 @@
 
 // Second Try
 
-const firstSection = document.querySelector('.first');
-const secondSection = document.querySelector('.second');
+// const firstSection = document.querySelector('.first');
+// const secondSection = document.querySelector('.second');
 
-let backgroundY = 0
+// let backgroundY = 0
 
-window.addEventListener('load', () => {
-    backgroundY = secondSection.offsetTop;
-})
+// window.addEventListener('load', () => {
+//     backgroundY = secondSection.offsetTop;
+// })
 
-window.addEventListener('scroll', () => {
-    const scroll = window.scrollY
+// window.addEventListener('scroll', () => {
+//     const scroll = window.scrollY
 
-    firstSection.style.top = scroll / 10 + 'px'
+//     firstSection.style.top = scroll / 10 + 'px'
 
-    secondSection.style.top = backgroundY - scroll + 'px'
-})
+//     secondSection.style.top = backgroundY - scroll + 'px'
+// })
