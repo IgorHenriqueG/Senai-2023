7da2b4a7108319f49c1174d6c8d47c30d486f5da
IgorHenriqueG <igorhgoncalvess@gmail.com>
2024-05-13 11:29:02 -0300

    autogit

---
+++ 2Des/02-pwfe/Aula13/Quiz/questions.js
@@ -0,0 +1,42 @@
+
+var questions = [
+    {
+        type: "select",
+        text: "Quantas casas decimais tem o número pi?",
+        options: ["Duas", "Centenas", "Milhares", "Infinitas", ],
+        answer: 3
+    },
+    {
+        type: "select",
+        text: "Qual e o maior planeta do sistema solar?",
+        options: ["Mercurio", "Venus", "Terra", "Marte", "Jupiter", "Saturno", "Urano", "Netuno"],
+        answer: 4
+    },
+    {
+        type: "select",
+        text: "Qual e o menor planeta do sistema solar?",
+        options: ["Mercurio", "Venus", "Terra", "Marte", "Jupiter", "Saturno", "Urano", "Netuno"],
+        answer: 0
+    },
+    {
+        type: "text",
+        text: "Quem foi o primeiro homem a pisar na Lua?",
+        answer: "neil armstrong"
+    },
+    {
+        type: "text",
+        text: "Qual o planeta do sistema solar mais proximo do Sol?",
+        answer: "mercurio"
+    },
+    {
+        type: "text",
+        text: "Qual o planeta do sistema solar mais distante do Sol?",
+        answer: "netuno"
+    },
+    {
+        type: "option",
+        text: "Qual é o nome do ",
+    }
+]
+
+export default questions
