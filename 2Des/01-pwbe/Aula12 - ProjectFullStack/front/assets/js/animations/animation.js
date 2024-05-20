5e00c3f6679fed3854cbe53d295810585b8d1849
IgorHenriqueG <igorhgoncalvess@gmail.com>
2024-05-20 11:28:49 -0300

    autogit

--- 2Des/01-pwbe/Aula12 - ProjectFullStack/front/assets/js/animations/animation.js
+++ 2Des/01-pwbe/Aula12 - ProjectFullStack/front/assets/js/animations/animation.js
@@ -1,5 +1,5 @@
 // gsap.fromTo(".logo", { opacity: 0, y: -40 }, { opacity: 1, y: 0, duration: 1.5, ease: "power1.inOut" });
-gsap.fromTo(".loading-screen", { opacity: 1 }, { opacity: 0, duration: 1.5, delay: 2.5, ease: "power1.inOut", onComplete: function() { document.querySelector(".loading-screen").classList.add("hidden") }});
+gsap.fromTo(".loading-screen", { opacity: 1 }, { opacity: 0, duration: 1.5, delay: 2.8, ease: "power1.inOut", onComplete: function() { document.querySelector(".loading-screen").classList.add("hidden") }});
 gsap.fromTo(".logo h1", { opacity: 0, x: -40 }, { opacity: 1, x: 0, duration: 1.5, delay: 1.5, ease: "power1.inOut" });
 gsap.fromTo(".logo p", { opacity: 0, x: 40 }, { opacity: 1, x: 0, duration: 1.5, delay: 1.5, ease: "power1.inOut" });
 gsap.fromTo(".logo img", { opacity: 0, rotate: -260, y: "-200%" }, { opacity: 1, rotate: 0, y: 0, duration: 1.5, ease: "power1.inOut" });
@@ -10,13 +10,13 @@ gsap.fromTo(".logo img", { opacity: 0, rotate: -260, y: "-200%" }, { opacity: 1,
 
 // Parallax
 
-// gsap.to(".parallax-cloud", {
-//     scrollTrigger: {
-//       scrub: 15
-//     }, 
-//     y: (i, target) => -ScrollTrigger.maxScroll(window) * target.dataset.speed,
-//     ease: "none"
-// });
+gsap.to(".parallax-cloud", {
+    scrollTrigger: {
+      scrub: 1
+    }, 
+    y: (i, target) => -ScrollTrigger.maxScroll(window) * target.dataset.speed,
+    ease: "none"
+});
 
 window.addEventListener("scroll", function() {
    document.querySelector('header').classList.toggle("not-top", window.scrollY > 0);
